56d8afe9dd5a49b9c4ae7667bf68671415aa5887
Alienguy500 <alienguy500@gmail.com>
2024-11-25 11:48:01 +0000

    Update handTrack.py

--- handTrack.py
+++ handTrack.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-import mediapipe
+import mediapipe as mp
 
 cv2.namedWindow("preview")
 vc = cv2.VideoCapture(0)
@@ -21,38 +21,23 @@ def flip_image(array,axis = "y"):
         array = array[::-1]
         rotated_array = array[::-1,::-1]
         return rotated_array
-    
+
 def invert_colours(array):
-    for i in range(len(array)):
-        red = array[i][0]
-        green = array[i][1]
-        blue = array[i][2]
-
-        red = 255-red
-        green = 255-green
-        blue = 255-blue
-
-        array[i][0] = red
-        array[i][1] = green
-        array[i][2] = blue
-    return array
+    return np.invert(array)
 
 def overlayTwoArrays(array1,array2):
-    arr = array1
-    for i in range(len(array1)):
-        for j in range(len(array1[i])):
-            array1[i][j][0] = (array1[i][j][0]+array2[i][j][0])//2
-            array1[i][j][1] = (array1[i][j][1]+array2[i][j][1])//2
-            array1[i][j][2] = (array1[i][j][2]+array2[i][j][2])//2
-    return arr
+    return np.bitwise_and(array1,array2)
 
 image_is_reversed = False
+previous_frame = flip_image(np_frame)
 
 while rval:
     np_frame = np.asarray(frame)
     reversed_np_frame = flip_image(np_frame,"y")
-    image_is_reversed = not image_is_reversed
-    cv2.imshow("preview",overlayTwoArrays(np_frame,reversed_np_frame))
+    
+    reversed_np_frame = overlayTwoArrays(flip_image(np_frame),previous_frame)
+    cv2.imshow("preview",reversed_np_frame)
+    previous_frame = flip_image(np_frame)
     rval, frame = vc.read()
     key = cv2.waitKey(20)
     if key == 27: # exit on ESC
