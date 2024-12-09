479e8f90f429f917e1573fd59f3455fa9345a278
Alienguy500 <alienguy500@gmail.com>
2024-12-09 16:06:31 +0000

    Updated gesture detection
    now with thumbs!

--- app/utils/HandTracking/handTrack.py
+++ app/utils/HandTracking/handTrack.py
@@ -9,6 +9,37 @@ import math
 
 from app.utils.Sockets.SocketSend import send_message
 
+def get_angle_3_points(point_a, point_b, point_c):
+    a = math.sqrt((point_c.x - point_b.x) ** 2 + (point_c.y - point_b.y) ** 2)
+    b = math.sqrt((point_c.x - point_a.x) ** 2 + (point_c.y - point_a.y) ** 2)
+    c = math.sqrt((point_b.x - point_a.x) ** 2 + (point_b.y - point_a.y) ** 2)
+
+    return math.degrees(math.acos((a ** 2 + c ** 2 - b ** 2) / (2 * a * c)))
+
+class Finger:
+    def __init__(self, a, b, c, d, is_thumb):
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
+        self.is_thumb = is_thumb
+
+    def is_extended(self, wrist):
+        """
+        Returns True if the selected digit is extended
+
+        finger_tip, finger_mcp, and wrist all have x, y, and z attributes
+        """
+        if self.is_thumb:
+            angle_1 = get_angle_3_points(wrist, self.a, self.b)  # Angle at THUMB_CMC, landmarks[1]
+            angle_2 = get_angle_3_points(self.a, self.b, self.c)  # Angle at THUMB_MCP, landmarks[2]
+            angle_3 = get_angle_3_points(self.b, self.c, self.d)  # Angle at THUMB_IP, landmarks[3]
+            threshold = 160  # If any of the angles are below this number, the thumb is bent and therefore not extended fully (can be adjusted if it's too sensitive or not enough)
+            return angle_1 >= threshold and angle_2 >= threshold and angle_3 >= threshold  # Return True if all angles are above the threshold
+        else:
+            angle_a = get_angle_3_points(self.d, self.a, wrist)
+            return angle_a > 90
+
 class Gesture:
     def __init__(self, name, orientation, fingers):
         self.name = name # String containing the name of the gesture, e.g. "thumb up"
@@ -90,7 +121,7 @@ class HandTrackingMain:
                         self.gesture = self.detect_gestures(landmarks)
 
                     if self.gesture:
-                        image_flipped = cv2.putText(image_flipped, "Fuck you", (50, 50), self.font, 1, (255, 0, 255), 2,
+                        image_flipped = cv2.putText(image_flipped, "Thumb up", (50, 50), self.font, 1, (255, 0, 255), 2,
                                                     cv2.LINE_AA)
                         if not self.rising_edge:
                             self.rising_edge = True
@@ -106,43 +137,44 @@ class HandTrackingMain:
         cv2.destroyWindow("preview")
         self.vc.release()
 
-    def getAngle3Points(self, point_a,point_b,point_c):
+    def get_angle_3_points(self, point_a,point_b,point_c):
         a = math.sqrt((point_c.x-point_b.x)**2+(point_c.y-point_b.y)**2)
         b = math.sqrt((point_c.x-point_a.x)**2+(point_c.y-point_a.y)**2)
         c = math.sqrt((point_b.x-point_a.x)**2+(point_b.y-point_a.y)**2)
 
         return math.degrees(math.acos((a**2+c**2-b**2)/(2*a*c)))
 
-    def isExtended(self,finger_tip,finger_mcp,wrist):
+    def is_extended(self, tip, mcp, wrist, is_thumb = False, thumb_cmc = None, thumb_ip = None):
         """
-        Returns False if the finger_tip is between the finger metacarpal and the wrist,
-        otherwise it returns True
+        Returns True if the selected digit is extended
         
         finger_tip, finger_mcp, and wrist all have x, y, and z attributes
         """
-        angle_A = self.getAngle3Points(finger_tip,finger_mcp,wrist)
-        return angle_A > 90
+        if is_thumb:
+            angle_1 = self.get_angle_3_points(wrist, thumb_cmc, mcp) # Angle at THUMB_CMC, landmarks[1]
+            angle_2 = self.get_angle_3_points(thumb_cmc, mcp, thumb_ip) # Angle at THUMB_MCP, landmarks[2]
+            angle_3 = self.get_angle_3_points(mcp, thumb_ip, tip) # Angle at THUMB_IP, landmarks[3]
+            threshold = 135 # If any of the angles are below this number, the thumb is bent and therefore not extended fully (can be adjusted if it's too sensitive or not enough)
+            return angle_1 >= threshold and angle_2 >= threshold and angle_3 >= threshold # Return True if all angles are above the threshold
+        else:
+            angle_a = self.get_angle_3_points(tip,mcp,wrist)
+            return angle_a > 90
     
     def detect_gestures(self, landmarks):
         """
         Detect what hand gestures are being shown
         """
         wrist = landmarks[0]
-        thumb_mcp = landmarks[2]
-        thumb_tip = landmarks[4]
-        index_mcp = landmarks[5]
-        index_tip = landmarks[8]
-        middle_mcp = landmarks[9]
-        middle_tip = landmarks[12]
-        ring_mcp = landmarks[13]
-        ring_tip = landmarks[16]
-        pinky_mcp = landmarks[17]
-        pinky_tip = landmarks[20]
-        if self.isExtended(middle_tip, middle_mcp, wrist) and not (
-                self.isExtended(index_tip, index_mcp, wrist) or self.isExtended(ring_tip, ring_mcp,
-                                                                                wrist) or self.isExtended(pinky_tip,
-                                                                                                          pinky_mcp,
-                                                                                                          wrist)):
+        f_thumb = Finger(landmarks[1],landmarks[2],landmarks[3],landmarks[4], True).is_extended(wrist)
+        f_index = Finger(landmarks[5],landmarks[6],landmarks[7],landmarks[8], False).is_extended(wrist)
+        f_middle = Finger(landmarks[9],landmarks[10],landmarks[11],landmarks[12], False).is_extended(wrist)
+        f_ring = Finger(landmarks[13],landmarks[14],landmarks[15],landmarks[16], False).is_extended(wrist)
+        f_pinky = Finger(landmarks[17],landmarks[18],landmarks[19],landmarks[20], False).is_extended(wrist)
+
+        hand = [wrist, f_thumb, f_index, f_middle, f_ring, f_pinky]
+
+        #if f_middle.is_extended(wrist) and not (f_thumb.is_extended(wrist) or f_index.is_extended(wrist) or f_ring.is_extended(wrist) or f_pinky.is_extended(wrist)):
+        if f_thumb and not (f_index or f_middle or f_ring or f_pinky):
             return True
         else:
             return False
